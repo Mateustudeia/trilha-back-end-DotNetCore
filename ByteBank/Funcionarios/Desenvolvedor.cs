5fb17409fc36417818433325e0dbac0edc0e722c
Mateus <mateustudeia@gmail.com>
2019-09-20 16:49:03 -0300

    Iniciando o repositorio do curso de CSharpe

---
+++ ByteBank/Funcionarios/Desenvolvedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Desenvolvedor : Funcionario
+    {
+        public Desenvolvedor(string cpf) : base(cpf, 7000)
+        {
+
+        }
+
+        public override void AumentarSalario()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double GetBonificacao()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
